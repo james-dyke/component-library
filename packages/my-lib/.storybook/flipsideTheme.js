8f73ad68865deb8edcbaad80836dc35fcce5865c
James Dyke <jcdyke@gmail.com>
2022-07-08 15:56:30 -0700

    FS specific theme for SB

---
+++ packages/my-lib/.storybook/flipsideTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Flipside Crypto',
+  brandUrl: 'https://flipsidecrypto.xyz/',
+  brandImage: 'https://avatars.githubusercontent.com/u/32752226?s=50',
+  brandTarget: '_self',
+  fontBase: '"Inter", sans-serif',
+});
